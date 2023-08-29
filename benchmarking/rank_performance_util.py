d5d11354ec694f43b4e0be2d45527a89167feedf
Real-Doris-Mae <135761538+Real-Doris-Mae@users.noreply.github.com>
2023-08-29 08:29:58 +0800

    Update rank_performance_util.py

--- benchmarking/rank_performance_util.py
+++ benchmarking/rank_performance_util.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 import pdb
+import random
+from tqdm import tqdm
 
 # Precision at k: It measures the proportion of recommended items in the top-k set that are relevant
 def precision_at_k(relevance_list, ranking_result, k=20):
@@ -18,6 +20,7 @@ def precision_at_k(relevance_list, ranking_result, k=20):
     ranking_set = set(ranking_result)
     inter_size = len(relevance_set.intersection(ranking_set))
     return inter_size / k
+
 
 # Recall at k: It measures the proportion of relevant items found in the top-k recommendations
 def recall_at_k(relevance_list, ranking_result, k=20):
@@ -37,6 +40,7 @@ def recall_at_k(relevance_list, ranking_result, k=20):
     inter_size = len(relevance_set.intersection(ranking_set))
     return inter_size / len(relevance_list)
 
+
 def r_precision(relevance_list, ranking_result):
     """
     Parameters:
@@ -53,6 +57,7 @@ def r_precision(relevance_list, ranking_result):
 
     inter_size = len(relevance_set.intersection(ranking_set))
     return inter_size / relevance_len
+
 
 # Normalized Discounted Cumulative Gain (NDCG): A measure of ranking quality. It uses the graded relevance of a query result set and discounts the relevance of documents lower down in the result list.
 def ndcg_normal(ground_truth_ranking, ground_truth_score, ranking_result, p):
@@ -80,6 +85,7 @@ def ndcg_normal(ground_truth_ranking, ground_truth_score, ranking_result, p):
         idcg += rel_i / math.log2(ind + 2)
     return dcg / idcg
 
+
 # Alternate version of NDCG: Similar to NDCG, but this version uses a exponential gain to emphasise the importance of relevance.
 def ndcg_exp(ground_truth_ranking, ground_truth_score, ranking_result, p):
     """
@@ -105,6 +111,7 @@ def ndcg_exp(ground_truth_ranking, ground_truth_score, ranking_result, p):
         idcg += (2 ** rel_i) / math.log2(ind + 2)
     return dcg / idcg
 
+
 # Reciprocal Rank: The reciprocal of the rank of the first relevant document
 def reciprocal_rank(ground_truth_rank, ground_truth_score, ranking_result, k):
     """
@@ -128,6 +135,7 @@ def reciprocal_rank(ground_truth_rank, ground_truth_score, ranking_result, k):
         if visible_ranking_result[i] in relevance_docs:
             return 1 / (i + 1)
     return 0
+
 
 def average_precision(relevance_list, ranking_result):
     """
@@ -158,6 +166,62 @@ def sort_dict_by_values(dictionary):
     return sorted_dict
 
 
+def generate_random_rank(rank_template, seed):
+    random.seed(seed)
+    randomized_rank = []
+    for d in rank_template:
+        temp_rank = d['index_rank'].copy()
+        random.shuffle(temp_rank)
+        randomized_rank.append({'index_rank': temp_rank})
+
+    return randomized_rank
+
+
+def get_random_baseline(gpt_result, rank, test_suites, trials=1000):
+    '''
+    Parameters: 
+           gpt_result: List of results from GPT model.
+           test_suites: test metrics
+           rank: A result rank dictionary for performing random baseline
+           trials: number of trials for performing random baseline
+    Return:
+          returning rank, rank is a list of dictionary, the position index corresponds to the number of query, and in each dicitonary, the only key is index_rank, the value is a list of paper abstarct ids sorted in descending order w.r.t. their relevance of the given query, based on the current random basline. 
+    '''
+    metric_result_dict = {}
+    for i in tqdm(range(trials), desc="Generating random trails", leave=False):
+        curr_seed = i
+        curr_random_rank = generate_random_rank(rank, curr_seed)
+        for test_name in test_suites.keys():
+            for k in test_suites[test_name]:
+                value = get_metric(gpt_result, curr_random_rank, test_name, k)
+                if (test_name, k) not in metric_result_dict.keys():
+                    metric_result_dict[(test_name, k)] = [value]
+                else:
+                    metric_result_dict[(test_name, k)].append(value)
+
+    return metric_result_dict
+
+
+def print_random_baseline(test_suites, metric_result_dict):
+    '''
+    Parameters: 
+           test_suites: test metrics
+           metric_result_dict: A result rank dictionary for random baseline
+    '''
+    model_name = "random"
+    print()
+    print("Random metrics:")
+    for test_name in test_suites.keys():
+        for k in test_suites[test_name]:
+            value = np.mean(metric_result_dict[(test_name, k)])
+            if k != None:
+                print(f"{model_name} : {test_name}@{k}  : {value}%")
+            else:
+                print(f"{model_name} : {test_name}  : {value}%")
+
+    print('=' * 100)
+
+
 def get_metric(gpt_result, method_rank, metric_name, k=None):
     """
     This function calculates various IR metrics including Recall, R Precision, NDCG, NDCG_exp, MRR, and MAP.
@@ -174,8 +238,12 @@ def get_metric(gpt_result, method_rank, metric_name, k=None):
     Raises:
         RuntimeError: If 'k' is not provided where required, or metric_name is not recognized.
     """
-    assert len(gpt_result) == len(method_rank)
-
+    assert len(gpt_result) == len(method_rank), f"{len(gpt_result)}; {len(method_rank)}"
+    removed_list = [205580, 346695, 346826, 346836, 28674]
+    
+    # threshold for determining relevant list
+    threshold = 1.0
+    
     relevance_lists = []
     original_gpt_result = []
 
@@ -191,8 +259,10 @@ def get_metric(gpt_result, method_rank, metric_name, k=None):
         curr_relevance_list = []
         for abs_id in i.keys():
             curr_normalized_score = i[abs_id][1]
-            if curr_normalized_score >= 1.0:
+            if curr_normalized_score >= threshold:
                 curr_relevance_list.append(abs_id)
+
+            curr_relevance_list = [c for c in curr_relevance_list if c not in removed_list]
         relevance_lists.append(curr_relevance_list)
 
     if metric_name == "recall":
@@ -207,7 +277,7 @@ def get_metric(gpt_result, method_rank, metric_name, k=None):
             if len(curr_relevance_list) == 0:
                 continue
             ret_list.append(recall_at_k(curr_relevance_list, curr_ranking_result, k))
-        return np.mean(ret_list)*100
+        return np.mean(ret_list) * 100
 
     elif metric_name == 'r_precision':
         ret_list = []
@@ -218,11 +288,11 @@ def get_metric(gpt_result, method_rank, metric_name, k=None):
             if len(curr_relevance_list) == 0:
                 continue
             ret_list.append(r_precision(curr_relevance_list, curr_ranking_result))
-        return np.mean(ret_list)*100
+        return np.mean(ret_list) * 100
 
     elif metric_name == 'ndcg':
         ret_list = []
-        if k is None or k <= 0 or k >= 1:
+        if k is None or k <= 0 or k > 1:
             raise RuntimeError("k should be between 0 and 1 for NDCG")
 
         for ind in range(len(method_rank)):
@@ -232,11 +302,11 @@ def get_metric(gpt_result, method_rank, metric_name, k=None):
             p = k
 
             ret_list.append(ndcg_normal(curr_gpt_rank, curr_gpt_score, curr_ranking_result, p))
-        return np.mean(ret_list)*100
+        return np.mean(ret_list) * 100
 
     elif metric_name == 'ndcg_exp':
         ret_list = []
-        if k is None or k <= 0 or k >= 1:
+        if k is None or k <= 0 or k > 1:
             raise RuntimeError("k should be between 0 and 1 for NDCG")
 
         for ind in range(len(method_rank)):
@@ -246,7 +316,7 @@ def get_metric(gpt_result, method_rank, metric_name, k=None):
             p = k
 
             ret_list.append(ndcg_exp(curr_gpt_rank, curr_gpt_score, curr_ranking_result, p))
-        return np.mean(ret_list)*100
+        return np.mean(ret_list) * 100
 
     elif metric_name == 'mrr':
         ret_list = []
@@ -258,7 +328,7 @@ def get_metric(gpt_result, method_rank, metric_name, k=None):
             curr_gpt_score = list(original_gpt_result[ind].values())
             curr_ranking_result = method_rank[ind]['index_rank']
             ret_list.append(reciprocal_rank(curr_gpt_rank, curr_gpt_score, curr_ranking_result, k))
-        return np.mean(ret_list)*100
+        return np.mean(ret_list) * 100
 
     elif metric_name == 'map':
         ret_list = []
@@ -269,7 +339,159 @@ def get_metric(gpt_result, method_rank, metric_name, k=None):
             if len(curr_relevance_list) == 0:
                 continue
             ret_list.append(average_precision(curr_relevance_list, curr_ranking_result))
-        return np.mean(ret_list)*100
+        return np.mean(ret_list) * 100
+
+    elif metric_name == 'binary_acc':
+
+        ret_list = []
+
+        for ind in range(len(method_rank)):
+
+            curr_relevance_list = relevance_lists[ind]
+            curr_ranking_result = method_rank[ind]['index_rank']
+
+            if len(curr_relevance_list) <= 1:
+                continue
+            curr_gpt_rank = list(original_gpt_result[ind].keys())
+            ret_list.append(binary_classification(curr_relevance_list, curr_ranking_result, curr_gpt_rank))
+        return np.mean(ret_list) * 100
+
+    else:
+        raise RuntimeError("metric_name should be one of ['recall', 'r_precision', 'ndcg', 'ndcg_exp', 'mrr', 'map']")
+        
+def get_full_metric_results(gpt_result, method_rank, metric_name, k=None):
+    """
+    This function calculates various IR metrics including Recall, R Precision, NDCG, NDCG_exp, MRR, and MAP.
+
+    Parameters:
+        gpt_result (list): List of results from GPT model.
+        method_rank (list): List of ranked document ids.
+        metric_name (str): The name of the metric to be calculated.
+        k (int, optional): An optional parameter for some metrics (default is None).
+
+    Returns:
+        float: Mean of the computed metric across all queries.
+
+    Raises:
+        RuntimeError: If 'k' is not provided where required, or metric_name is not recognized.
+    """
+    assert len(gpt_result) == len(method_rank)
+    removed_list = [205580, 346695, 346826, 346836, 28674]
+    
+    # threshold for determining relevant list
+    threshold = 1.0
+    
+    relevance_lists = []
+    original_gpt_result = []
+
+    for i in gpt_result:
+        curr_original_gpt_result = {}
+        for abs_id in i.keys():
+            curr_original_gpt_result[abs_id] = i[abs_id][0]
+
+        curr_original_gpt_result = sort_dict_by_values(curr_original_gpt_result)
+        original_gpt_result.append(curr_original_gpt_result)
+
+    for i in gpt_result:
+        curr_relevance_list = []
+        for abs_id in i.keys():
+            curr_normalized_score = i[abs_id][1]
+            if curr_normalized_score >= threshold:
+                curr_relevance_list.append(abs_id)
+
+            curr_relevance_list = [c for c in curr_relevance_list if c not in removed_list]
+        relevance_lists.append(curr_relevance_list)
+
+    if metric_name == "recall":
+        if k is None or k <= 0:
+            raise RuntimeError("k should be positive for recall")
+
+        ret_list = []
+
+        for ind in range(len(method_rank)):
+            curr_relevance_list = relevance_lists[ind]
+            curr_ranking_result = method_rank[ind]['index_rank']
+            if len(curr_relevance_list) == 0:
+                continue
+            ret_list.append(recall_at_k(curr_relevance_list, curr_ranking_result, k))
+        return ret_list
+
+    elif metric_name == 'r_precision':
+        ret_list = []
+
+        for ind in range(len(method_rank)):
+            curr_relevance_list = relevance_lists[ind]
+            curr_ranking_result = method_rank[ind]['index_rank']
+            if len(curr_relevance_list) == 0:
+                continue
+            ret_list.append(r_precision(curr_relevance_list, curr_ranking_result))
+        return ret_list
+
+    elif metric_name == 'ndcg':
+        ret_list = []
+        if k is None or k <= 0 or k > 1:
+            raise RuntimeError("k should be between 0 and 1 for NDCG")
+
+        for ind in range(len(method_rank)):
+            curr_gpt_rank = list(original_gpt_result[ind].keys())
+            curr_gpt_score = list(original_gpt_result[ind].values())
+            curr_ranking_result = method_rank[ind]['index_rank']
+            p = k
+
+            ret_list.append(ndcg_normal(curr_gpt_rank, curr_gpt_score, curr_ranking_result, p))
+        return ret_list
+
+    elif metric_name == 'ndcg_exp':
+        ret_list = []
+        if k is None or k <= 0 or k > 1:
+            raise RuntimeError("k should be between 0 and 1 for NDCG")
+
+        for ind in range(len(method_rank)):
+            curr_gpt_rank = list(original_gpt_result[ind].keys())
+            curr_gpt_score = list(original_gpt_result[ind].values())
+            curr_ranking_result = method_rank[ind]['index_rank']
+            p = k
+
+            ret_list.append(ndcg_exp(curr_gpt_rank, curr_gpt_score, curr_ranking_result, p))
+        return ret_list
+
+    elif metric_name == 'mrr':
+        ret_list = []
+        if k is None or k <= 0:
+            raise RuntimeError("k should be positive for MRR")
+
+        for ind in range(len(method_rank)):
+            curr_gpt_rank = list(original_gpt_result[ind].keys())
+            curr_gpt_score = list(original_gpt_result[ind].values())
+            curr_ranking_result = method_rank[ind]['index_rank']
+            ret_list.append(reciprocal_rank(curr_gpt_rank, curr_gpt_score, curr_ranking_result, k))
+        return ret_list
+
+    elif metric_name == 'map':
+        ret_list = []
+
+        for ind in range(len(method_rank)):
+            curr_relevance_list = relevance_lists[ind]
+            curr_ranking_result = method_rank[ind]['index_rank']
+            if len(curr_relevance_list) == 0:
+                continue
+            ret_list.append(average_precision(curr_relevance_list, curr_ranking_result))
+        return ret_list
+
+    elif metric_name == 'binary_acc':
+
+        ret_list = []
+
+        for ind in range(len(method_rank)):
+
+            curr_relevance_list = relevance_lists[ind]
+            curr_ranking_result = method_rank[ind]['index_rank']
+
+            if len(curr_relevance_list) <= 1:
+                continue
+            curr_gpt_rank = list(original_gpt_result[ind].keys())
+            ret_list.append(binary_classification(curr_relevance_list, curr_ranking_result, curr_gpt_rank))
+        return ret_list
 
     else:
         raise RuntimeError("metric_name should be one of ['recall', 'r_precision', 'ndcg', 'ndcg_exp', 'mrr', 'map']")
