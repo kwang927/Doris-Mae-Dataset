08abd8a8f60c63f71a20ba2e9620b9b1017e7c91
Real-Doris-Mae <135761538+Real-Doris-Mae@users.noreply.github.com>
2023-08-30 19:09:01 +0800

    Update run.sh

--- benchmarking/run.sh
+++ benchmarking/run.sh
@@ -2,7 +2,9 @@
 pip install -r pre_requirements.txt #installing requirements
 python3 modifications/combine_lfs.py
 
-wget -P dataset https://zenodo.org/record/8035110/files/DORIS_MAE_dataset_v0.json  # downloading doris mae dataset v0. 
+wget -P dataset https://zenodo.org/record/8299749/files/DORIS-MAE_dataset_v1.json # downloading doris mae dataset v1.
+wget -P dataset https://zenodo.org/record/8299749/files/ada_embedding_for_DORIS-MAE_v1.pickle # downloading ada embedding of the dataset.
+
 
 git clone https://github.com/stanford-futuredata/ColBERT.git # downloading colbertv2 model
 wget https://downloads.cs.stanford.edu/nlp/data/colbert/colbertv2/colbertv2.0.tar.gz
